66805c134125d957d7c7a0234e6b46e6aa1fa17f
I. Backus <ibackus@login2.hyak.local>
2014-06-10 12:04:53 -0700

    Revert "Print statements added for profiling"
    This reverts commit a6ae05c13666b83a1f1a8707fe21972bd1f758d9.

--- walltime.py
+++ walltime.py
@@ -5,16 +5,10 @@ Created on Fri Mar 14 15:25:36 2014
 @author: ibackus
 """
 
-import time
-
-t0 = time.time()
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 import sys
-
-t1 = time.time()
-print 'Importing took {} s'.format(t1-t0)
 
 if len(sys.argv) < 2:
     
@@ -33,9 +27,4 @@ else:
     print str(walltime_avg)
     
     plt.plot(log_file[:,-1],'x')
-
-    t2 = time.time()
-    print 'Running took an extra {} s'.format(t2-t1)
-    print 'For a total of {} s'.format(t2 - t0)
-    
     plt.show()
