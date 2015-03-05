0b7c2e64fe296e4be0f9b81cd72e1bcb8512f387
I. Backus <ibackus@login2.hyak.local>
2015-03-04 16:18:41 -0800

    Changed how make_continue_sub works.  No longer loops through directories and the arguments have been simplified.  directory looping should be handled by the program calling it

--- changastuff.py
+++ changastuff.py
@@ -182,8 +182,8 @@ def subber(directories, scriptname, scriptstr=None):
     # Return to current working directory
     os.chdir(cwd)
 
-def make_continue_sub(simdirs='.', paramname='snapshot.param', \
-newparam='continue.param', simtime=1, nSteps=None, oldsub='subber.sh', \
+def make_continue_sub(simdir='.', paramname='snapshot.param', \
+newparam='continue.param', nSteps=None, nSteps_extra=None, oldsub='subber.sh', \
 newsub='cont_subber.sh'):
     """
     Makes a submission script for continuing a simulation from a previous output.
@@ -192,107 +192,108 @@ newsub='cont_subber.sh'):
     for outputs being the same.
     
     Parameters for the original simulation cannot be altered (except the number
-    of steps you want to continue the simulation by)
+    of steps you want to continue the simulation by).  PBS runtime parameters
+    also cannot be changed (number of nodes, walltime, etc...)
     
     Any checkpoints will be deleted.
     
     Requires a submission script be present for the original simulation
     
+    NOTE: if nSteps, nSteps_extra are not set, the total number of steps
+    to simulate is not changed.
+    
     
     **ARGUMENTS**
     
-    simdirs : str or iterable containing strings
-        The directory/directories containing the simulation(s) to continue
+    simdir : str
+        The simulation directory
     paramname : str
-        Filename of the .param file for the simulation(s).  Must be the same 
-        for every simulation
+        Filename of the .param file for the simulation
     newparam : str
         filename for the .param file for the continued simulation
-    simtime : float
-        How many times the original nSteps to continue by.  IE, the new total
-        number of steps (including the original simulation) is calculated as:
-            nSteps_new = nSteps_old * (simtime + 1)
-        if simtime=0, then the simulation will just continue and finish and
-        the original number of steps.  (similar to checkpointing)
     nSteps : int
-        OVERRIDES simtime.  Sets the total number of extra steps to simulate.    
+        Total number of steps to simulate
+    nSteps_extra : int
+        Instead of total number of steps this sets how many more to complete
+        OVERIDES nSteps!!!
     oldsub : str
-        Filename for the original submission script (should be the same for
-        every simdir)
+        Filename for the original submission script
     newsub : str
-        Filename for the new submission script (should be the same for every 
-        simdir)
+        Filename for the new submission script
+        
+    **RETURNS**
+    
+    sub_path : str
+        Full path to the PBS submission script
     
     """
     
     # Lazy man's way of dealing with files in another directory
     cwd = os.getcwd()
     
-    # Make simdirs a list if needed
-    if isinstance(simdirs, str):
+    os.chdir(simdir)
+    
+    # Load param file
+    param = isaac.configparser(paramname, 'param')
+    fprefix = param['achOutName']
+    
+    # Find all the outputs.  They should be of the format fprefix.000000
+    search_exp = '^' + fprefix + '.(?:(?<!\d)\d{6}(?!\d))$'
+    flist = []
+    
+    for fname in glob.glob(fprefix + '*'):
         
-        simdirs = [simdirs]
-        
-    # Iterate over all the simulation directories present
-    for simdir in simdirs:
-        
-        os.chdir(simdir)
-        
-        # Load param file
-        param = isaac.configparser(paramname, 'param')
-        fprefix = param['achOutName']
-        
-        # Find all the outputs.  They should be of the format fprefix.000000
-        search_exp = '^' + fprefix + '.(?:(?<!\d)\d{6}(?!\d))$'
-        flist = []
-        
-        for fname in glob.glob(fprefix + '*'):
+        if re.search(search_exp, fname) is not None:
             
-            if re.search(search_exp, fname) is not None:
-                
-                flist.append(fname)
+            flist.append(fname)
+    
+    # Find the number of the last output (the last 6 chars should be an int)
+    flist.sort()
+    iStartStep = int(flist[-1][-6:])
+    param['iStartStep'] = iStartStep
+    param['achInFile'] = flist[-1]    
+    
+    # Set the number of steps to run        
+    if nSteps_extra is not None:
         
-        # Find the number of the last output (the last 6 chars should be an int)
-        flist.sort()
-        iStartStep = int(flist[-1][-6:])
-        param['iStartStep'] = iStartStep
-        param['achInFile'] = flist[-1]    
+        param['nSteps'] = iStartStep + nSteps_extra
         
-        # Set the number of steps to run
-        if nSteps is None:
-            
-            nSteps = int(param['nSteps'] * simtime)
-            
-        param['nSteps'] = iStartStep + nSteps
+    elif nSteps is not None:
         
-        # Save new param file
-        isaac.configsave(param, newparam, ftype='param')
-        
-        # Delete old checkpointssubmitted 8 hours ago by Aikukun to /r/gifs
+        param['nSteps'] = nSteps
+    
+    # Save new param file
+    isaac.configsave(param, newparam, ftype='param')
+    
+    # Delete old checkpoints
 
-        for checkpoint in glob.glob(fprefix + '.chk*'):
-            
-            print 'removing ' + checkpoint
-            os.system('rm -rf ' + checkpoint)
-            
-        if os.path.exists('lastcheckpoint'):
-            
-            print 'removing lastcheckpoint'
-            os.remove('lastcheckpoint')
+    for checkpoint in glob.glob(fprefix + '.chk*'):
         
-        # Create a submission script for the simulation continuation
-        oldsubfile = open(oldsub, 'r')
-        newsubfile = open(newsub, 'w')
+        print 'removing ' + checkpoint
+        os.system('rm -rf ' + checkpoint)
         
-        for line in oldsubfile:
-            
-            newsubfile.write(line.replace(paramname, newparam))
-            
-        oldsubfile.close()
-        newsubfile.close()
+    if os.path.exists('lastcheckpoint'):
         
-        # Make the submission script executable
-        os.chmod(newsub, 0777)
+        print 'removing lastcheckpoint'
+        os.remove('lastcheckpoint')
+    
+    # Create a submission script for the simulation continuation
+    oldsubfile = open(oldsub, 'r')
+    newsubfile = open(newsub, 'w')
+    
+    for line in oldsubfile:
         
-        # Change back to original working directory
-        os.chdir(cwd)
+        newsubfile.write(line.replace(paramname, newparam))
+        
+    oldsubfile.close()
+    newsubfile.close()
+    
+    # Make the submission script executable
+    os.chmod(newsub, 0777)
+    
+    sub_path = os.path.abspath(newsub)
+    
+    # Change back to original working directory
+    os.chdir(cwd)
+    
+    return sub_path
