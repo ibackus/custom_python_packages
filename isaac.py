0ab46734eff8b0b14907a9b03b32e34861de6b0b
Isaac Backus <ibackus@gmail.com>
2015-03-30 16:58:54 -0400

    Added snapshot_defaults

--- isaac.py
+++ isaac.py
@@ -20,6 +20,34 @@ import fnmatch
 self_dir = os.path.dirname(os.path.realpath(__file__))
 print os.path.realpath(__file__)
 
+def snapshot_defaults(snapshot):
+    """
+    Applies various defaults to tipsy snapshots of protoplanetary disk
+    simulations. These include:
+    
+        -Sets nice units
+        -Calculates particle smoothing lengths using mass and rho (if available)
+    
+    Changes the snapshot in place
+    """
+    
+    # Setup units
+    snapshot['pos'].convert_units('au')
+    snapshot['mass'].convert_units('Msol')
+    snapshot['vel'].convert_units('km s**-1')
+    snapshot['eps'].convert_units('au')
+    snapshot.g['temp'].convert_units('K')
+    snapshot.g['rho'].convert_units('Msol au**-3')
+    
+    # Calculate smoothing lengths
+    if ('rho' in snapshot.g.loadable_keys()) or ('rho' in snapshot.g.keys()):
+        
+        if ~(np.any(snapshot.g['rho'] == 0)):
+            
+            snapshot.g['smooth'] = (snapshot.g['mass']/snapshot.g['rho'])**(1,3)
+    
+    return
+
 def units_from_param(param):
     """
     Figures out the simulation units from a .param file
