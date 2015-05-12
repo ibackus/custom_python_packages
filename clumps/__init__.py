d96d4cdc1f13978492fa89e4c1222d6edfd0223c
Isaac Backus <ibackus@gmail.com>
2015-05-12 00:17:36 -0700

    Implemented new classes for holding clump data for lots of simulations (in simclumps.py)

--- clumps/__init__.py
+++ clumps/__init__.py
@@ -10,3 +10,5 @@ Created on Mon May 11 23:28:56 2015
 from clumpfinding import clump_tracker, get_fnames, blank_clump, build_clumps,\
 multilink, pLink2, link2, clump_im, pClump_properties, clump_properties, \
 pFind_clumps, find_clumps
+
+import simclumps
